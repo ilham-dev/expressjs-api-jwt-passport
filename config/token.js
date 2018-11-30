efd7a1cf0e5414a91f9fc84eea76035c9c1e4e47
M ilham sabar <60200111048@uin-alauddin.ac.id>
2018-11-30 14:37:23 +0800

    reformat code

--- config/token.js
+++ config/token.js
@@ -1,16 +1,16 @@
 getToken = function (headers) {
-    if (headers && headers.authorization) {
-      var parted = headers.authorization.split(' ');
-      if (parted.length === 2) {
-        return parted[1];
-      } else {
-        console.log('Unauthorized1');
-        return null;
-      }
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
     } else {
-        console.log('Unauthorized');
+      console.log('Unauthorized1');
       return null;
     }
-  };
+  } else {
+    console.log('Unauthorized');
+    return null;
+  }
+};
 
-  module.exports = getToken;
+module.exports = getToken;
